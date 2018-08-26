9e03973cdca7d26c1dae6efb6b2886edfae32f03
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-26 10:01:05 -0600

    Adds in config file, updated user_socket file, and Procfile for deploying to Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,19 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :quantified_self_phoenix, QuantifiedSelfPhoenixWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "peaceful-dusk-38517.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarde_proto]],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Configure database for production
+config :quantified_self_phoenix, QuantifiedSelfPhoenix.Repo,
+  adapter: Ecto.Adapter.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-# Configure database for production
-# config :quantified_self_phoenix, QuantifiedSelfPhoenix.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   username: "postgres",
-#   password: "postgres",
-#   database: "quantified_self_phoenix_prod",
-#   pool_size: 15
 
 # ## SSL Support
 #
@@ -67,6 +67,3 @@ config :logger, level: :info
 #     config :quantified_self_phoenix, QuantifiedSelfPhoenixWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
--- lib/quantified_self_phoenix_web/channels/user_socket.ex
+++ lib/quantified_self_phoenix_web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule QuantifiedSelfPhoenixWeb.UserSocket do
   # channel "room:*", QuantifiedSelfPhoenixWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
