38499a00d4b172edfa09ac3776b44b93ee34a0f6
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-27 08:36:11 -0600

    Adds describe blocks to food_requests test and updates verbosity of tests

--- test/quantified_self_phoenix_web/requests/food_requests_test.exs
+++ test/quantified_self_phoenix_web/requests/food_requests_test.exs
@@ -10,30 +10,34 @@ defmodule QuantifiedSelfPhoenixWeb.FoodRequestsTest do
     :ok
   end
 
-  test "GET /api/v1/foods", %{conn: conn} do
-    expected = [
-      %{name: "Tacos", calories: 900},
-      %{name: "Dumplings", calories: 1000}
-    ]
-
-    response = get conn, "/api/v1/foods"
-    body = json_response(response, 200)
-
-    assert length(body) == length(expected)
-    assert Enum.at(body, 0)["name"] == Enum.at(expected, 0)[:name]
-    assert Enum.at(body, 0)["calories"] == Enum.at(expected, 0)[:calories]
-    
-    assert Enum.at(body, 1)["name"] == Enum.at(expected, 1)[:name]
-    assert Enum.at(body, 1)["calories"] == Enum.at(expected, 1)[:calories]
+  describe "GET /api/v1/foods" do
+    test "should all persisted foods from the foods table", %{conn: conn} do
+      expected = [
+        %{name: "Tacos", calories: 900},
+        %{name: "Dumplings", calories: 1000}
+      ]
+  
+      response = get conn, "/api/v1/foods"
+      body = json_response(response, 200)
+  
+      assert length(body) == length(expected)
+      assert Enum.at(body, 0)["name"] == Enum.at(expected, 0)[:name]
+      assert Enum.at(body, 0)["calories"] == Enum.at(expected, 0)[:calories]
+      
+      assert Enum.at(body, 1)["name"] == Enum.at(expected, 1)[:name]
+      assert Enum.at(body, 1)["calories"] == Enum.at(expected, 1)[:calories]
+    end
   end
-
-  test "GET /api/v1/foods/:id", %{conn: conn} do
-    expected = %{name: "Dumplings", calories: 1000}
-
-    response = get conn, "/api/v1/foods/2"
-    body = json_response(response, 200)
-
-    assert body["name"] == expected[:name]
-    assert body["calories"] == expected[:calories]
+  
+  describe "GET /api/v1/foods/:id" do
+    test "should return the food specified by the given id", %{conn: conn} do
+      expected = %{name: "Dumplings", calories: 1000}
+  
+      response = get conn, "/api/v1/foods/2"
+      body = json_response(response, 200)
+  
+      assert body["name"] == expected[:name]
+      assert body["calories"] == expected[:calories]
+    end
   end
 end
